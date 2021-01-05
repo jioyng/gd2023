fd8c0aa8451a28f56cb2efc9fd6802c8ce0506a0
hzy1001 <hzy1001@moumou.co.kr>
2021-01-05 18:00:55 +0900

    서버와클라이언트간의 통신 테스트

--- myNode/nodejs.js
+++ myNode/nodejs.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", onPageLoaded, false);
+ 
 
  function onPageLoaded(){  
       
      var socket = io.connect("//127.0.0.1:9892");
 
-    //  socket.on("getusername",function(name){
-    //      alert("내이름은"+name);
-    //  });
+     //이게 외안되지 ??
+     socket.on("get_user_data",function(name){
+          alert("내이름은" + name + "입니다.");
+     });
     
  }
--- myNode/socket.io.server.js
+++ myNode/socket.io.server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var fs = require('fs');   //File System 함수를 node js에 가져오는 방법은 require('fs'); 이다. 출처: https://dydals5678.tistory.com/96 [아빠개발자의 노트]
+const { isObject } = require("util");
 //var socketio = require("socket.io");  ==> 이와같이하면 socketio 모듈을 직접 핸들하게 되고, 에러가 발생한다. 아래에 var socketio = require("socket.io")(server); 로 변경
 
 //노드 웹서버 실행
@@ -21,9 +22,32 @@ server.listen(9892, function(){
 //socketio모듈을 인스턴스화 하여 핸들해야한다
 var socketio = require("socket.io")(server); //참조url: https://stackoverflow.com/questions/41623528/io-on-is-not-a-function
 
-socketio.on("connection", function(socket)
+var io = socketio.listen( server ); 
+
+//socketio.on("connection", function(socket)
+io.on("connection", function(socket)
 {
     console.log('Socket Client Connected 크크크')
 })
+
+//접속자 랜덤
+function RandomNextInt(max)
+{
+    return 1 + Math.floor( Math.random() * max);
+}
+
+//접속자
+var names = ['길동','동수','승기','채윤','지완'];
+
+//클라이언트 접속
+io.sockets.on("connection", function( socket )
+{
+    var random = RandomNextInt(5) - 1;
+
+    console.log("[Client Connected] name : " + names[random]);
+
+    socket.emit("get_user_data", names[random]);
+});
+
 
  

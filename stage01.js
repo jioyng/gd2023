6e45689628ebca334e76581803f937708f430ed3
hzy1001 <hzy1001@naver.com>
2022-11-26 01:47:44 +0900

    ㅅㄷㄴㅅ

--- stage01.js
+++ stage01.js
@@ -5841,12 +5841,12 @@ function player_collision(){
             //console.log("Pdistance",Pdistance)  
                                 
                 //1초간 진동 
-                //navigator.vibrate(500); // 진동을 울리게 한다. 1000ms = 1s 이다.
+                navigator.vibrate(500); // 진동을 울리게 한다. 1000ms = 1s 이다.
 
                   //충돌시 폭파이미지로 변경
                 if (player_life <= 1){  
                     //2초간 진동 
-                    navigator.vibrate(500); // 진동을 울리게 한다. 1000ms = 1s 이다.
+                    navigator.vibrate(1000); // 진동을 울리게 한다. 1000ms = 1s 이다.
 
                     // explosion_sound.currentTime  = 4;
                     // explosion_sound.play();  
@@ -5960,8 +5960,8 @@ function player_collision(){
                      
                     //목소리 재생모드일경우만 실행  
                     if (ls_VColor == "yellow") {
-                        //3초간 진동 
-                        //navigator.vibrate(3000); // 진동을 울리게 한다. 1000ms = 1s 이다.
+                        //2초간 진동 
+                        navigator.vibrate(2000); // 진동을 울리게 한다. 1000ms = 1s 이다.
                         vfinish_sound.play(); 
                         //대화
                         Context.globalAlpha = 1;

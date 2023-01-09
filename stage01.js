bcdff3f9a98116c7259e1fad341576870118f36f
hzy1001 <hzy1001@gmail.com>
2023-01-09 22:12:37 +0900

    no message

--- stage01.js
+++ stage01.js
@@ -3196,14 +3196,14 @@ Context.rotate(backgroundY/2000*Math.PI/180);
             }    
         //     //Context.translate(-10,-10);
         // }else {
-            Context.globalAlpha = 0.5;
+            Context.globalAlpha = 0.3;
             //backgroundX = backgroundX - 1;
             //backgroundY = backgroundY - 1;  
-            backgroundWidth = backgroundWidth - 2.5; 
-            backgroundHeight = backgroundHeight - 0.2;     
+            backgroundWidth = backgroundWidth - 3; 
+            backgroundHeight = backgroundHeight - 0.1;     
         //}     
     }else {
-        Context.globalAlpha = 0.6; 
+        Context.globalAlpha = 0.5; 
         backgroundX = backgroundX + 0.5;
         backgroundY = backgroundY + 0.5;          
         backgroundWidth = backgroundWidth - 1;

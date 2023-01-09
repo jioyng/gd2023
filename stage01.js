4b3538b085445056b0ca962ed2781b2735baefbd
hzy1001 <hzy1001@gmail.com>
2023-01-09 22:00:28 +0900

    1111

--- stage01.js
+++ stage01.js
@@ -3199,8 +3199,8 @@ Context.rotate(backgroundY/2000*Math.PI/180);
             Context.globalAlpha = 0.5;
             //backgroundX = backgroundX - 1;
             //backgroundY = backgroundY - 1;  
-            backgroundWidth = backgroundWidth - 4; 
-            backgroundHeight = backgroundHeight - 1;     
+            backgroundWidth = backgroundWidth - 2.5; 
+            backgroundHeight = backgroundHeight - 0.5;     
         //}     
     }else {
         Context.globalAlpha = 0.6; 
